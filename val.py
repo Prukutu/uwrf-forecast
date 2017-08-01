949b9dc7f9afa0630c2f1de6953173f9a8ae1926
prukutu <luis.ortiz.uriarte@gmail.com>
2017-08-01 15:12:18 -0400

    Re-factor of the validation module.

--- val.py
+++ val.py
@@ -1,5 +1,10 @@
 import numpy as np
 from datetime import datetime
+# from qc import qualityControl
+
+# A set of utilities to read data from NYC-MetNet stations while doing some
+# quality control.
+
 
 """ Data key:
     ['DataKey',
@@ -44,7 +49,7 @@ class metnetCSV:
                 self.coords[ID] = (float(l[-3]), float(l[-4]))
 
     def extractStationData(self, stationID, varname, datatime,
-                           order='nearest', all=False):
+                           order='nearest', allvals=False):
 
         """ Extract a variable given by varname from the NYCMetNet CSV file
             object. The data is interpolated using method specified in order.
@@ -87,35 +92,109 @@ class metnetCSV:
                 data = [float(st[10]) for st in stationlines if
                         st[3] == stationID]
                 data = np.asarray(data)
-                data[data < -100] = np.nan
 
             elif varname == 'winds':
                 data = [float(st[12]) for st in stationlines if
                         st[3] == stationID]
                 data = np.asarray(data)
-                data[data < -100] = np.nan
+                # data[data < -100] = np.nan
 
-        if all is False:
-            if mindiff < 3600:
+        # Check that the neirest neighbor is not too far.
+        if allvals is False:
+            if mindiff < 3000:
                 return timeobj[minloc], data[minloc]
             else:
-                return np.nan, np.nan
+                return datatime, np.nan
         else:
             return timeobj, data
 
 
-def main():
-    import matplotlib.pyplot as plt
-    station = metnetCSV('20160405')
-    tm = datetime(2016, 4, 4, 15, 0)
-    timestamps, data = station.extractStationData('KNYC ',
-                                                  'temperature',
-                                                  tm)
+class validityCheck:
+    """ Class to check that station data is within valid range."""
 
-    fig, ax = plt.subplots()
-    ax.plot(timestamps, data)
-    fig.autofmt_xdate()
-    fig.savefig('test.png')
+    def __init__(self, rawdata, varname):
 
-if __name__ == '__main__':
-    main()
+        # Define range of acceptable values per variable.
+        self.thresh = {'temperature': [-5, 45],
+                       'winds': [0, 50]}
+        self.rawdata = rawdata
+        self.varname = varname
+
+    def run(self):
+
+        lowval, hival = self.thresh[self.varname]
+        lvl1data = self.rawdata
+
+        # Get the data's count of pre-existing NaNs
+        # print lvl1data
+        lvl1data = [data if (data >= lowval) and (data <= hival) else
+                    np.nan for data in self.rawdata]
+
+        nancount = self.rawdata.count(np.nan)
+        newnancount = lvl1data.count(np.nan) - nancount
+        print 'QC purged ' + str(newnancount) + ' data points!'
+
+        return lvl1data
+#
+#
+# class temporalCheck(qualityControl):
+#     def __init__(self, rawdata, varname):
+#         qualityControl.__init__(self, rawdata, varname)
+#         self.rate = {'temperature': 19.4444,
+#                      'winds': 10.2889}
+#         self.datamean = np.nanmean(self.rawdata)
+#
+#     def run(self, maxdev=15):
+#         datarate = self.rate[self.varname]
+#         lvl2data = [t for t in self.rawdata]
+#         hival = self.datamean + maxdev
+#         lowval = self.datamean - maxdev
+#
+#         print lowval, hival
+#in station.coords.keys():
+#     fig, ax1 = plt.subplots(figsize=(7, 4))
+#
+#     # Use colored twin axes to denote the data.
+#     ax2 = plt.twinx(ax=ax1)
+#     ax1.plot_date(timestamps, t2lvl1[key],
+#                   color='#f44336',
+#                   linestyle='-',
+#                   marker=None)
+#     ax2.plot_date(timestamps, wmaglvl1[key],
+#                   color='#009688',
+#                   linestyle='-',
+#                   marker=None)
+#
+#     ax1.set_ylabel(u'Temperature (\u2103)')
+#     ax1.set_ylim(-5, 42)
+#     ax1.yaxis.label.set_color('#f44336')
+#     ax2.spines['left'].set_color('#f44336')
+#     ax1.tick_params(axis='y', colors='#f44336')
+#
+#     ax2.set_ylabel('Wind Speed (m/s)')
+#     ax2.set_ylim(0, 10)
+#     ax2.yaxis.label.set_color('#009688')
+#     ax2.spines['right'].set_color('#009688')
+#     ax2.tick_params(axis='y', colors='#009688')
+#
+#     fig.autofmt_xdate()
+#
+#     fig.savefig(key + '.png', dpi=200, bbox_inches='tight')
+#     fig.close()
+
+#         for n in range(1, len(lvl2data)):
+#
+#             if np.isnan(lvl2data[n]):
+#                 lvl2data[n] = np.nan
+#
+#             elif (np.isnan(lvl2data[n-1]) and lvl2data[n] > lowval and
+#                   lvl2data[n] < hival):
+#                 lvl2data[n] = lvl2data[n]
+#
+#             elif abs(lvl2data[n] - lvl2data[n-1]) < datarate:
+#                 lvl2data[n] = lvl2data[n]
+#
+#             else:
+#                 lvl2data[n] = np.nan
+#
+#         return lvl2data
